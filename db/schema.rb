14dab0f8cd289904091cfe2bf5bc619515ba5939
Roger Curley <rocurley@gmail.com>
2012-09-02 10:14:35 -0700

    Merge

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(:version => 20120902170128) do
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"
-    t.integer  "chorons"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.integer  "chorons"
