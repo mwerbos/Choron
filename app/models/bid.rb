38fc78063fffa39c948a6cf6ababd1fbddb91ceb
Roger Curley <rocurley@gmail.com>
2012-09-02 23:51:58 -0700

    Fixed bid validation bug where an auction with no bids could not be bid upon.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -10,7 +10,7 @@ class Bid < ActiveRecord::Base
     def lowest
         if auction
             lowest_bid=self.auction.lowest
-            if self.amount>=lowest_bid
+            if lowest_bid and self.amount>=lowest_bid
                 errors[:base] << "The bid must be less than #{lowest_bid}"
             end
         end
