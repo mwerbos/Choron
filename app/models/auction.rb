39502a1712560c30b4a1b2a0302d1ff97f1554e8
Roger Curley <rocurley@gmail.com>
2012-09-04 15:31:58 -0700

    Bids list now hides exact top bid, to encourage bidding the least you'd do it for.

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,8 +1,10 @@
 class Auction < ActiveRecord::Base
+  include ApplicationHelper
   attr_accessible :expiration_date, :chore_id #explicit use is a bit hacky, oh well
   belongs_to :chore, :counter_cache => true
   has_many :bids
   def lowest()
-      self.bids.map{|x| x.amount}.min
+      sorted_bids=self.bids.sort {|a,b| bid_sorter(a,b)}
+      [sorted_bids[0].amount,sorted_bids[1].amount-1].max
   end
 end
