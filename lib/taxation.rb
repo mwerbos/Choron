f24daf0805a8074364c9f884b0735fc782f4e87a
Roger Curley <rocurley@gmail.com>
2013-06-25 01:56:26 -0400

    I believe this will fix the choron leak, and negative collective.

--- lib/taxation.rb
+++ lib/taxation.rb
@@ -1,14 +1,13 @@
 module Taxation
   def take_tax(recipient,amount)
-    #This doesn't behave quite like it should; chorons are conserved, but
-    #the collective sometimes gets negative amounts.
+    #I believe that I've fixed the choron leak and the negative collective.
     User.transaction do
       payers=User.where('id not in(?)',[recipient.id])
       numPayers=payers.length
       File.open("#{Rails.root}/testlog.txt", 'w') {|f| f.write(Setting.collective) }
       #This is negative; it will be added to everyone's chorons.
       tax=(-amount+Setting.collective)/numPayers
-      Setting.collective=-(amount+tax*numPayers)
+      Setting.collective+=-(amount+tax*numPayers)
       recipient.chorons+=amount
       recipient.save
       payers.each do |payer|
