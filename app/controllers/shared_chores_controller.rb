55a9ed38c5d5bf5b7af774b51bb5e48016e78af7
Ozzie Fallick <ozymandiasthegreat@gmail.com>
2013-07-01 22:38:30 -0400

    Implemented contribution

--- app/controllers/shared_chores_controller.rb
+++ app/controllers/shared_chores_controller.rb
@@ -1,5 +1,9 @@
 class SharedChoresController < ChoresController
   def contribute
+    chore = Chore.find(params[:id])
+    user = params[:user]
+    chore.contributions[User.where(:username => user)[0]].push Time.now
+    chore.save
     redirect_to(:back)
   end
 
