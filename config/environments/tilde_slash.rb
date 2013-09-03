37d28ff89c9a28d92ebf96988ca241b2a577b3e4
Maia <pi@raspberrypi.(none)>
2013-09-03 01:55:11 +0000

    Modified environment scripts and messed with relative url roots

--- config/environments/tilde_slash.rb
+++ config/environments/tilde_slash.rb
@@ -65,4 +65,8 @@ Choron::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Run from relative url root /tilde_slash
+  # config.action_controller.relative_url_root = '/tilde_slash'
+  config.relative_url_root = '/tilde_slash'
 end
--- run_chore_market_nginx.sh
+++ run_chore_market_nginx.sh
@@ -7,21 +7,31 @@ echo "Starting nginx..."
 sudo nginx -c "${PWD}/nginx/nginx.conf"
 
 ## Runs the ~/ chore market
+echo "For the ~/ chore market:"
 
 # Precompile assets: Only necessary if CSS/layouts have changed recently.
-# echo "Precompiling assets..."
-# bundle exec rake assets:precompile -e tilde_slash
+#echo "Precompiling assets..."
+#RAILS_ENV=tilde_slash bundle exec rake assets:precompile 
 
 echo "Starting unicorn server..."
-unicorn_rails -c config/unicorn.rb -E tilde_slash -D
+RAILS_RELATIVE_URL_ROOT='/tilde_slash' unicorn_rails -c config/unicorn_tilde_slash.rb -E tilde_slash -D
 
 echo "Starting rake jobs:work..."
 echo "Nevermind, rake jobs:work is disabled for devl reasons."
 # rake jobs:work RAILS_ENV=tilde_slash &
 
-# TODO get both to run at once
-## Runs the castle hauser chore market
-# unicorn_rails -c config/unicorn.rb -E castle_hauser -D
+## Runs the hauser chore market
+echo "For the Castle Hauser chore market:"
+
+# Precompile assets: Only necessary if CSS/layouts have changed recently.
+#echo "Precompiling assets..."
+#RAILS_ENV=castle_hauser bundle exec rake assets:precompile 
+
+echo "Starting unicorn server..."
+RAILS_RELATIVE_URL_ROOT='/castle_hauser' unicorn_rails -c config/unicorn_castle_hauser.rb -E castle_hauser -D
+
+echo "Starting rake jobs:work..."
+echo "Nevermind, rake jobs:work is disabled for devl reasons."
 # rake jobs:work RAILS_ENV=castle_hauser &
 
 # How to kill these processes:
